240bf4031eb0684ae8aa6741348ff104059b846b
truongthanhinfo <157579121+tiemgiamgia@users.noreply.github.com>
2024-01-27 19:10:19 +0700

    Update

---
+++ src/components/Footer.tsx
@@ -0,0 +1,20 @@
+const Footer = () => {
+  return (
+    <footer className="bg-neutral-50 border-t border-neutral-200">
+      <div className="max-w-6xl mx-auto px-5 p-10">
+        <h3 className="font-semibold text-2xl mb-10 lg:mb-0 lg:pr-4">
+          Bản quyền thuộc{' '}
+          <a className="underline" href="https://tiemgiamgia.vercel.app/">
+            Tiệm Giảm Giá Vercel
+          </a>{' '}
+          thành viên của{' '}
+          <a className="underline" href="https://tiemgiamgia.com/">
+            Tiệm Giảm Giá
+          </a>
+        </h3>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
