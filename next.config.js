d9ac120730004bc2462460411c8d59f0e4203c35
truongthanhinfo <150412884+truongthanhinfo@users.noreply.github.com>
2024-05-28 12:12:17 +0700

    Delete next.config.js

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  assetPrefix: process.env.ASSET_PREFIX,
-  basePath: process.env.BASE_PATH,
-};
-
-module.exports = nextConfig;
