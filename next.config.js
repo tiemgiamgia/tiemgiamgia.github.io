05aa698e655fd9335da859094b7ebc1af26e290a
truongthanhinfo <150412884+truongthanhinfo@users.noreply.github.com>
2024-05-28 11:30:35 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/tiemgiamgia.github.io",
+  basePath: "tiemgiamgia/tiemgiamgia.github.io",
   output: "export",
   reactStrictMode: true,
 };
